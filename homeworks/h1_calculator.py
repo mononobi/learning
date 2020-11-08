4ae00e6584984baefb7bf5d6e6733f0efe71f02e
mono <mohamadnobakht@gmail.com>
2020-11-08 21:41:27 +0330

    Revise code

--- homeworks/h1_calculator.py
+++ homeworks/h1_calculator.py
@@ -1,17 +1,18 @@
 # -*- coding: utf-8 -*-
+"""
+define four main functions:
 
-#define four main functions:
+1. add: which takes two required arguments.
+2. subtract: which takes two required arguments.
+3. divide: which takes two required arguments.
+4. multiply: which takes two required arguments.
 
-#1. add: which takes two required arguments.
-#2. subtract: which takes two required arguments.
-#3. divide: which takes two required arguments.
-#4. multiply: which takes two required arguments.
-
-#define a calculator function which takes two required numbers and an
-#optional string operator. if operator is not provided, it must be
-#considered as 'add'.
-#calculator function must return the result of each operation.
-#then you should print the result of calculator method.
+define a calculator function which takes two required numbers and an
+optional string operator. if operator is not provided, it must be
+considered as 'add'.
+calculator function must return the result of each operation.
+then you should print the result of calculator method.
+"""
 
 
 def sum(num1, num2):
