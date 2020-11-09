b757585f4fac8395d733c9b3923634b0c3d1d173
mono <mohamadnobakht@gmail.com>
2020-11-09 23:08:44 +0330

    Session 3

--- homeworks/h2_sequence.py
+++ homeworks/h2_sequence.py
@@ -2,11 +2,11 @@
 """
 we have a list of first names and another list of last names.
 
-1. write a concatenator method to loop through the list with fewer items
+1. write a concatenate method to loop through the list with fewer items
    and concatenate each first name to last name of the same index, the
    loop must be ended when the shorter list is exhausted.
-2. write a method to show the list with maximum length.
-3. write a method to sort the result list ascending.
+2. write a method to print the list with maximum length.
+3. write a method to sort the first_names ascending and last_names descending.
 """
 
 first_names = ['azin', 'abi', 'tarek', 'sheida', 'mohamad',
