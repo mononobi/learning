dbb03988d10bb7eba7344f32a207fba280619ccc
Sheida <sheida.nobakht@gmail.com>
2020-11-15 14:30:13 +0100

    Assignments 2

--- homeworks/h2_sequence.py
+++ homeworks/h2_sequence.py
@@ -9,14 +9,95 @@ we have a list of first names and another list of last names.
 3. write a method to sort the first_names ascending and last_names descending.
 """
 
-first_names = ['azin', 'abi', 'tarek', 'sheida', 'mohamad',
-               'shiva', 'saleh', 'andy', 'reza', 'faeze', 'azin']
+first_names = ['azin', 'tarek', 'sheida', 'mohamad',
+               'shiva', 'faeze', 'saleh', 'andy', 'reza', 'abi', 'azin']
 
 last_names = ['kamali', 'krohn', 'nobakht', 'nobakht', 'roostaei', 'fallah', 'noorian']
 
 
+###### Excersice 1 ######
+
+
 def get_shorter_list(a, b):
-    """
-    return the shorter input list.
-    """
-    pass
+    """gets two lists and returns the shorter one.
+     If both are the same length, returns the first list"""
+    len_a = len(a)
+    len_b = len(b)
+    if len_a <= len_b:
+        min_list = a
+    else:
+        min_list = b
+    return min_list
+
+
+# test
+print('The shorter list is: ',  get_shorter_list(first_names, last_names))
+
+
+def match_names_v1(a, b):
+    """gets two lists and matches the first and last names
+     Uses the above method to finde the shorter list"""
+    ref_list = get_shorter_list(first_names, last_names)
+    for index, name in enumerate(ref_list):
+        print(index+1, a[index].capitalize(), b[index].capitalize())
+
+
+# test
+print('Matching by the first version of the function: ')
+print(match_names_v1(first_names, last_names))
+
+
+def match_names_v2(a, b):
+    """gets two lists and matches the first and last names
+     Finding the right (shorter) indexing happens within the function itself"""
+    for index, name in enumerate(a):
+        print(index + 1, a[index].capitalize(), b[index].capitalize())
+        if index >= min(len(a), len(b))-1:
+            break
+
+# test
+print('Matching by the second version of the function: ')
+print(match_names_v2(first_names, last_names))
+
+
+######  Excersice 2 ######
+
+def get_longer_list(a, b):
+    """gets two lists and returns the longer one
+     If they are both the same size, returns the first one"""
+    max_len = max(len(a), len(b))
+    max_list = b
+    if len(a) == max_len:
+        max_list = a
+    return max_list
+
+#test
+print('The longer List is: ', get_longer_list(first_names, last_names))
+
+
+###### Excercise 3 ######
+def sort_names1(a, b):
+    """sorts the lists
+     print the values of the lists one by one"""
+    a = sorted(a)
+    b = sorted(b, reverse=True)
+    print('First names: ')
+    for index, firstname in enumerate(a):
+        print(index+1, a[index])
+    print('Last names: ')
+    for index, lastname in enumerate(b):
+        print(index+1, b[index])
+
+#test
+print(sort_names1(first_names, last_names))
+
+
+def sort_names2(a, b):
+    """sorts the lists
+     prints each list as a whole list"""
+    a = sorted(a)
+    b = sorted(b, reverse=True)
+    return a, b
+
+#test
+print(sort_names2(first_names, last_names))
