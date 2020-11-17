b4d3c103a19f2e670465cd0974adefbc5b4c8ff7
mono <mohamadnobakht@gmail.com>
2020-11-17 23:33:13 +0330

    Add h3_list_tuple.py

---
+++ homeworks/h3_list_tuple.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+1. we have a list of numbers (float or integer) named 'numbers_list'.
+   define a function to process each number and separate even and
+   odd numbers into two separate lists (excluding float and negative numbers).
+
+2. define a function to make the equivalent tuple of odd and even
+   numbers lists from the first exercise. (search the web for help if needed)
+
+3. define a function to loop through the 'numbers_list' while reaching to a negative number.
+   then return the index of the negative number and the negative number itself and also
+   remove that number from the list.
+
+4. define a function to calculate the sum of numbers in each of even and odd numbers list.
+   (do not use loop for this, search the web if needed)
+"""
+
+numbers_list = [56, 8.5, 4, 33, 90, 1003, 100.2, 10.0, 34,
+                707, 606, 505, -19, 23, 44, 90, 67.6, -3.4]
