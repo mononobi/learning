cba103b515376e3cd816261e01b9d665a69d3867
Sheida <sheida.nobakht@gmail.com>
2022-01-16 18:06:58 +0100

    homework Smartphn-Advanced

--- homeworks/intermediate/h2_smartphone_advanced.py
+++ homeworks/intermediate/h2_smartphone_advanced.py
@@ -36,3 +36,144 @@ different types of smartphones lose charge at different rates:
 Make different smartphone classes and make instances of each one and use
 the common interface to interact with each phone.
 """
+
+
+
+import homeworks.intermediate.utils as utils
+
+
+class Smartphone:
+    def __init__(self, brand, model):
+        self._charge = 20
+        self._power_on = False
+        self._charge_per_minute = 0
+        self._usage_per_minute = 0
+        self._brand = brand
+        self._model = model
+
+    @property
+    def charge(self):
+        self._charge
+
+    @property
+    def power_on(self):
+        self._power_on
+
+    @charge_per_minute.setter
+    def charge_per_minute(self, value):
+        utils.assert_positive_number(value)
+        if value > 100:
+            raise Exception('Change more than 100 invalid')
+        else:
+            self._charge_per_minute = value
+
+    @usage_per_minute.setter
+    def usage_per_minute(self, value):
+        utils.assert_positive_number(value)
+        if value > 100:
+            raise Exception('Change more than 100 invalid')
+        else:
+            self._usage_per_minute = value
+
+    @staticmethod
+    def get_brand():
+        pass
+
+    @staticmethod
+    def get_model():
+        pass
+
+    def charge(self, minutes):
+        ##utils.assert_positive_number(minutes)
+        charged = round(minutes * self._charge_per_minute)
+        self._charge = min(self._charge + charged, 100)
+        message = 'Charged for {minutes} minutes. The battery level is {charge_level}%'
+        return self._charge
+
+    def play(self, minutes):
+        ##utils.assert_positive_number(minutes)
+        if not self._power_on:
+            raise Exception('The phone is off')
+        else:
+            used_charge = round(minutes * self._usage_per_minute)
+            if self._charge <= used_charge:
+                self._charge = 0
+                self._power_on = False
+                raise Exception('Battery not sufficient')
+            else:
+                self._charge = self._charge - used_charge
+                return self._charge
+
+    def turn_on(self):
+        if not self._power_on:
+            self._power_on = True
+        else:
+            raise Exception('The phone is already on')
+
+    def turn_off(self):
+        if self._power_on:
+            self._power_on = False
+        else:
+            raise Exception('The phone is already off')
+
+    def get_charge(self):
+        if self._power_on:
+            return self._charge
+        else:
+            raise Exception('The phone is off')
+
+
+
+
+
+class Samsung(Smartphone):
+    def __init__(self, model):
+        super().__init__('Samsung', model) ##???
+        #self._charge_per_minute = round(20/20, 2)
+        #self._usage_per_minute = round(10/20, 2)
+
+    charge_per_minute = round(20/20, 2)
+    usage_per_minute = round(10 / 20, 2)
+
+    @staticmethod
+    def get_brand():
+        return 'Samsung'
+
+    @staticmethod
+    def get_model():
+        pass
+
+
+class iPhone(Smartphone):
+    def __init__(self, model):
+        super().__init__('iPhone', model) ##???
+        #self._charge_per_minute = round(10/15, 2)
+        #self._usage_per_minute = round(15/20, 2)
+
+    charge_per_minute = round(10/15, 2)
+    usage_per_minute = round(15/20, 2)
+
+    @staticmethod
+    def get_brand():
+        return 'iPhone'
+
+    @staticmethod
+    def get_model():
+        pass
+
+class OnePlus(Smartphone):
+    def __init__(self, model):
+        super().__init__('iPhone', model) ###??
+        #self._charge_per_minute = round(25/30, 2)
+        #self._usage_per_minute = round(15/25, 2)
+
+    charge_per_minute = round(25 / 30, 2)
+    usage_per_minute = round(15 / 25, 2)
+
+    @staticmethod
+    def get_brand():
+        return 'OnePlus'
+
+    @staticmethod
+    def get_model():
+        pass ##??
